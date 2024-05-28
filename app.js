0052fa1da73434ec05935a8497365cc4ec6a2967
Jean <jean.bressan@hotmail.com>
2024-05-28 15:32:08 -0300

    Atualization 2

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
+botao.addEventListener("click", clicar)
+
 function consultaCep() {
    const cep = document.querySelector('#cep').value 
    const logradouro = document.querySelector('#logradouro')
-   const number = document.querySelector('#number') 
+   const complemento = document.querySelector('#complemento') 
    const bairro = document.querySelector('#bairro') 
-   const city = document.querySelector('#city')
-   const estado = document.querySelector('#estado') 
+   const cidade = document.querySelector('#cidade')
+   const uf = document.querySelector('#uf') 
 
    fetch(`https://viacep.com.br/ws/${cep}/json/`)
    .then(resposta => resposta.json())
-//    .then(json => console.log(json))
    .then(json => {
-     logradouro.value = JSON.stringify(json)
-     number.value = JSON.stringify(json)
-     bairro.value = JSON.stringify(json)
-     city.value = JSON.stringify(json)
-     estado.value = JSON.stringify(json)
+     logradouro.value = JSON.logradouro
+     complemento.value = JSON.complemento
+     bairro.value = JSON.bairro
+     cidade.value = JSON.cidade
+     uf.value = JSON.uf
    })
 
    .catch(error => {
     alert('O CEP que você inseriu, não foi encontrado! Tente novamente utilizando outro CEP.')
    });
 }
+
+const salvarLStorage = window.document.getElementById("salvarLStorage")
+salvarLStorage.addEventListener("click", salvarCep)
+
+function salvarCep(){
+   const cep = document.getElementById("resposta")
+   localStorage.setItem("cep", cep)
+   alert('Salvo com Sucesso!')
+}
+
+window.onload = salvarCep;
